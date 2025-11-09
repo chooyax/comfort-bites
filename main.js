5585942131113b297910ac9c3ed736c181035ab8
chooyax <xanzcrust@gmail.com>
2025-11-10 01:27:26 +1100

    Project - form validation

--- main.js
+++ main.js
@@ -16,3 +16,33 @@ hamburgerButton.addEventListener("click", () => {
     }
 
 });
+
+const appForm = document.querySelector('#application-form')
+const errorList = document.querySelector('#errors')
+const firstName = document.querySelector('#first-name')
+const lastName = document.querySelector('#last-name')
+const countrySelect = document.querySelector('#country')
+const subject = document.querySelector('#subject')
+
+const validation = (e) => {
+    e.preventDefault()
+    errorList.innerHTML = null
+    if (firstName.value.length < 2 || lastName.value.length < 2) {
+        const err = document.createElement('li')
+        err.textContent = "First and Last name must be at least 2 characters"
+        errorList.appendChild(err)
+    }
+    if (countrySelect.value === "Select One") {
+        const err = document.createElement('li')
+        err.textContent = "Please select a country"
+        errorList.appendChild(err)
+    }
+    let newString = subject.value.split(" ").join('')
+    if (newString.length < 50) {
+        const err = document.createElement('li')
+        err.textContent = "Message should be at least 20 characters"
+        errorList.appendChild(err)
+    }
+}
+
+appForm.addEventListener('submit', validation)
