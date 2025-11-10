cf6aea5c807041a93531a925b11fd035e02e381f
chooyax <xanzcrust@gmail.com>
2025-11-10 22:34:27 +1100

    Project - Form update

--- main.js
+++ main.js
@@ -23,26 +23,76 @@ const firstName = document.querySelector('#first-name')
 const lastName = document.querySelector('#last-name')
 const countrySelect = document.querySelector('#country')
 const subject = document.querySelector('#subject')
+const successMessageForForm = document.querySelector('#thank-you-message')
 
 const validation = (e) => {
     e.preventDefault()
+    
     errorList.innerHTML = null
+    let error = false;
+    
+    // checking if first name has less than 2 letters OR
+    // checking if last name has less than 2 letters
     if (firstName.value.length < 2 || lastName.value.length < 2) {
+        error = true;
         const err = document.createElement('li')
+        
         err.textContent = "First and Last name must be at least 2 characters"
         errorList.appendChild(err)
     }
+    
+    //checking if country value selected is "Select One"
     if (countrySelect.value === "Select One") {
+        error = true;
         const err = document.createElement('li')
+        
         err.textContent = "Please select a country"
         errorList.appendChild(err)
     }
+
     let newString = subject.value.split(" ").join('')
-    if (newString.length < 50) {
+    
+    //checking if all characters excluding spaces has less than 20 characters
+    if (newString.length < 20) {
+        error = true;
         const err = document.createElement('li')
+        
         err.textContent = "Message should be at least 20 characters"
         errorList.appendChild(err)
     }
+
+    if (error === false) {
+        firstName.value = "";
+        lastName.value = "";
+        subject.value = "";
+        countrySelect.selectedIndex = 0;
+
+        successMessageForForm.textContent = "Thank you for your queries"
+
+        setTimeout(function(){
+            successMessageForForm.textContent = "";
+        }, 5000);
+    }
+    error = false;
 }
 
 appForm.addEventListener('submit', validation)
+
+
+
+const subsForm = document.querySelector('#subscribe')
+const inputName = document.querySelector('#name')
+const inputEmail = document.querySelector('#email')
+
+const subscription = (e) => {
+   e.preventDefault()
+   errorList.innerHTML = null
+   if (inputName.value.length < 2) {
+        const err = document.createElement('li')
+        err.textContent = "Name must be at least 2 characters"
+        errorList.appendChild(err)
+    }
+
+}
+
+subsForm.addEventListener('submit', subscription)
